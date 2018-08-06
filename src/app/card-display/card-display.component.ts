29dd39b75795b023253b5d984fa0bb9bce9687f3
vkush248 <40993679+vkush248@users.noreply.github.com>
2018-08-06 20:39:19 +0300

    added simplified layout for cards and tested json file

--- src/app/card-display/card-display.component.ts
+++ src/app/card-display/card-display.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
-
+import * as data from "../../../db.json";
 @Component({
   selector: 'app-card-display',
   templateUrl: './card-display.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./card-display.component.css']
 })
-export class CardDisplayComponent {
-
+export class CardDisplayComponent implements OnInit{
+  cardsArray = data.cards;
   constructor() { }
 
-  @Input() cards;
+  // @Input() cards;
+  ngOnInit(){
+    console.log(this.cardsArray);
+  }
 }
