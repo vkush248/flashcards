ef491caa200a4de19a5427542bb1fab6ef853abd
vkush248 <40993679+vkush248@users.noreply.github.com>
2018-08-15 13:09:58 +0300

    fix bugs

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -5,10 +5,11 @@ import { CardDisplayComponent } from './card-display/card-display.component';
 import { CardItemComponent } from './card-item/card-item.component';
 import { CardUpdateComponent } from './card-update/card-update.component';
 import { routing } from './cards.routes';
+import { CardDetailsComponent } from './card-details/card-details.component';
 
 @NgModule({
   imports: [routing, CommonModule],
   providers: [CardService],
-  declarations: [CardDisplayComponent, CardUpdateComponent, CardItemComponent]
+  declarations: [CardDisplayComponent, CardUpdateComponent, CardItemComponent, CardDetailsComponent]
 })
 export class CardsModule {}
--- src/app/cards/cards.routes.ts
+++ src/app/cards/cards.routes.ts
@@ -1,11 +1,21 @@
 import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardDetailsComponent } from './card-details/card-details.component';
 import { CardDisplayComponent } from './card-display/card-display.component';
 import { CardUpdateComponent } from './card-update/card-update.component';
 
 const cardsRoutes: Routes = [
   { path: '', component: CardDisplayComponent },
-  { path: 'card', component: CardUpdateComponent }
+  { path: 'card', component: CardUpdateComponent },
+  {
+    path: 'card-details/:id',
+    component: CardDetailsComponent /* ,
+    children: [
+      { path: '', redirectTo: 'overview', pathMatch: 'full' },
+      { path: 'overview', component: 'OverviewComponent' },
+      { path: 'update', component: 'UpdateComponent' }
+    ] */
+  }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forChild(cardsRoutes);
