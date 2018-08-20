c739ebe20c985547b273c4bc8d6e7666aff60338
vkush248 <40993679+vkush248@users.noreply.github.com>
2018-08-20 13:26:39 +0300

    style: add material, nav styling, css reset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { routing } from './app.routes';
 import { HeaderComponent } from './header/header.component';
 import { cardsReducer } from './store/cards.reducer';
-
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     routing,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
     StoreModule.forRoot({ cardsStore: cardsReducer })
   ],
   bootstrap: [AppComponent]
