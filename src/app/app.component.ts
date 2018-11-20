65223a55b56c1743eeaac93911c74642c19e9bf4
Kushniruk Viktor <vkushniruk@exadel.by>
2018-11-20 11:48:32 +0200

    refactor nested pipes in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Actions, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { map, switchMap, take, throttleTime } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { ModalService } from './services/modal.service';
 import * as fromStore from './store';
 
@@ -12,15 +12,10 @@ import * as fromStore from './store';
 })
 export class AppComponent {
   constructor(actions$: Actions, private modalService: ModalService, private store: Store<fromStore.AppState>) {
-    actions$.pipe(ofType(fromStore.SELECT_SNACKBAR)).pipe(
-      throttleTime(500),
-      switchMap(() =>
-        this.store.select(fromStore.getSnackbarState)
-          .pipe(
-            map(message => this.modalService.openSnackbar(message)),
-            take(1)
-          )
-      )
+    actions$.pipe(
+      ofType(fromStore.SELECT_SNACKBAR),
+      map((action: any) => action.payload),
+      map(message => this.modalService.openSnackbar(message)),
     ).subscribe();
   }
 }
