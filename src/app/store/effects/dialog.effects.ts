f1c8753e25da8b0fdbbd39525a58d02855baf5dd
Kushniruk Viktor <vkushniruk@exadel.by>
2018-11-20 17:43:55 +0200

    refactor dialog effect

--- src/app/store/effects/dialog.effects.ts
+++ src/app/store/effects/dialog.effects.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { exhaustMap, map } from 'rxjs/operators';
+import { combineLatest, of } from 'rxjs';
+import { exhaustMap, map, switchMap } from 'rxjs/operators';
 import { ModalService } from 'src/app/services/modal.service';
 import * as cardsActions from '../../cards/store/actions';
 import * as dialogActions from '../actions/dialog.actions';
@@ -17,17 +18,18 @@ export class DialogEffects {
     ofType(dialogActions.OPEN_DIALOG),
     map((action: any) => action.payload),
     exhaustMap((payload: any) => {
-      return this.modalService.openDialog(payload.config).afterClosed().pipe(
-        map(agree => {
-          if (agree) {
-            const actionName = payload.action.name;
-            const actionPayload = payload.action.payload;
-            return new cardsActions[actionName](actionPayload);
-          } else {
-            return new dialogActions.CloseDialog();
-          }
-        }),
-      );
-    })
+      return combineLatest([this.modalService.openDialog(payload.config).afterClosed(), of(payload)]);
+    }),
+    switchMap(([agree, payload]): any => {
+      if (agree) {
+        const actionName = payload.action.name;
+        const actionPayload = payload.action.payload;
+        return [
+          new cardsActions[actionName](actionPayload),
+          new dialogActions.CloseDialog()
+        ];
+      }
+      return new dialogActions.CloseDialog();
+    }),
   );
 }
